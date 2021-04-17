b6202fe6c39bf91bd50a9b580e0a91a86aeb30f9
joy <joyhhh@outlook.kr>
2021-04-17 15:35:06 +0900

    before_Frame

--- JE_Tester/JE_Tester.py
+++ JE_Tester/JE_Tester.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import os
+import tkinter.ttk as ttk
 from tkinter import *
-from tkinter import ttk
-from tkinter.ttk import Progressbar
 from tkinter import filedialog
-import calendar
-import threading
-
 
 def readexcel():
     global filename
@@ -120,62 +116,153 @@ def B1():
 
 
 win =Tk()
-win.geometry("500x500")
+win.geometry("1000x800")
 win.title("JE Tester by Joy 0.1.0, Copyright 2021. Joy. All rights reserved ")
 win.option_add("*Font","NanumGothic 16")
 
+frame_file = Frame(win, relief = solid, bd = 1)
+frame_file.pack()
 
-l0 = Label(win, text = "Input")
+b0 = Button(win, text = "JE File Upload",command=readexcel)
+b0.grid(row=0, column=0, sticky = N+E+W+S, padx =10)
 
-b0 = Button(win, text = "File Upload",command=readexcel)
+sel_lab1 = Label(win, text = "Select ACCT Code")
+sel_lab1.grid(row=1, column=0, sticky = W, padx =10)
+ACCT = [str(i) for i in range(1,32)]
+combobox1 = ttk.Combobox(win,height =5, state="readonly", values = ACCT)
+combobox1.grid(row = 1, column = 1)
+combobox1.set("Select ACCT Code")
+
+sel_lab2 = Label(win, text = "Select JE No.")
+sel_lab2.grid(row=2, column=0, sticky = W, padx =10)
+JENO = [str(i) for i in range(1,32)]
+combobox2 = ttk.Combobox(win,height =5, state="readonly", values = JENO)
+combobox2.grid(row = 2, column = 1)
+combobox2.set("Select JE No.")
+
+sel_lab3 = Label(win, text = "Select JE Date")
+sel_lab3.grid(row=3, column=0, sticky = W, padx =10)
+JEDATE = [str(i) for i in range(1,32)]
+combobox3 = ttk.Combobox(win,height =5, state="readonly", values = JEDATE)
+combobox3.grid(row = 3, column = 1)
+combobox3.set("Select JE Date")
+
+sel_lab4 = Label(win, text = "Select Debit Record")
+sel_lab4.grid(row=4, column=0, sticky = W, padx =10)
+DR = [str(i) for i in range(1,32)]
+combobox4 = ttk.Combobox(win,height =5, state="readonly", values = DR)
+combobox4.grid(row = 4, column = 1)
+combobox4.set("Select DR")
+
+sel_lab5 = Label(win, text = "Select Credit Record")
+sel_lab5.grid(row=5, column=0, sticky = W, padx =10)
+CR = [str(i) for i in range(1,32)]
+combobox5 = ttk.Combobox(win,height =5, state="readonly", values = CR)
+combobox5.grid(row = 5, column = 1)
+combobox5.set("Select CR")
+
+
+
+b6 = Button(win, text = "TB(CY) File Upload",command=readexcel)  # command attach
+b6.grid(row=0, column=3, sticky = N+E+W+S, padx =10)
+
+ACCT_CYTB = [str(i) for i in range(1,32)]
+combobox7 = ttk.Combobox(win,height =5, state="readonly", values = ACCT_CYTB)
+combobox7.grid(row = 1, column = 3)
+combobox7.set("Select ACCT Code")
+
+ACCT_DRSUM = [str(i) for i in range(1,32)]
+combobox8 = ttk.Combobox(win,height =5, state="readonly", values = ACCT_DRSUM)
+combobox8.grid(row = 2, column = 3)
+combobox8.set("Select DR")
+
+ACCT_CRSUM = [str(i) for i in range(1,32)]
+combobox9 = ttk.Combobox(win,height =5, state="readonly", values = ACCT_CRSUM)
+combobox9.grid(row = 3, column = 3)
+combobox9.set("Select CR")
+
+b7 = Button(win, text = "TB(PY) File Upload",command=readexcel) # command attach
+b7.grid(row=0, column=4, sticky = N+E+W+S, padx =10)
+
+ACCT_PYTB = [str(i) for i in range(1,32)]
+combobox11 = ttk.Combobox(win,height =5, state="readonly", values = ACCT_PYTB)
+combobox11.grid(row = 1, column = 4)
+combobox11.set("Select ACCT Code")
+
+ACCT_DRSUM_PY = [str(i) for i in range(1,32)]
+combobox12 = ttk.Combobox(win,height =5, state="readonly", values = ACCT_DRSUM_PY)
+combobox12.grid(row = 2, column = 4)
+combobox12.set("Select DR")
+
+ACCT_CRSUM_PY = [str(i) for i in range(1,32)]
+combobox13 = ttk.Combobox(win,height =5, state="readonly", values = ACCT_CRSUM_PY)
+combobox13.grid(row = 3, column = 4)
+combobox13.set("Select CR")
+
+
+
+
+
 l2 = Label(win, text = "Beg date")
 l3 = Label(win, text = "End date")
-l4 = Label(win, text = "Date Set")
-l5 = Label(win, text = "NA Row")
-l6 = Label(win, text = "Acc Code")
+l4 = Label(win, text = "Date Format")
+l5 = Label(win, text = "Column for NA Lines")
+l6 = Label(win, text = "Account Code for B01")
 
-l0.grid(row=5, column=1)
-
-
-b0.grid(row=6, column=0, sticky = W, padx =10)
-l2.grid(row=7, column=0, sticky = W, padx =10)
-l3.grid(row=8, column=0, sticky = W, padx =10)
-l4.grid(row=9, column=0, sticky = W, padx =10)
-l5.grid(row=10, column=0, sticky = W, padx =10)
-l6.grid(row=11, column=0, sticky = W, padx =10)
+l2.grid(row=11, column=0, sticky = E, padx =10)
+l3.grid(row=12, column=0, sticky = E, padx =10)
+l4.grid(row=13, column=0, sticky = E, padx =10)
+l5.grid(row=14, column=0, sticky = E, padx =10)
+l6.grid(row=15, column=0, sticky = E, padx =10)
 
 
 
 lbl0 = Label(win, text ="JE file (Row, Col) : ? ")
-lbl0.grid(row=6, column=1, sticky = W,  pady = 20)
+lbl0.grid(row=0, column=1, sticky = N+E+W+S,  pady = 20)
 e2=Entry(win)
 e3=Entry(win)
-e4=Entry(win)
-e5=Entry(win)
-e6=Entry(win)
 
 
-e2.grid(row=7, column =1)
-e3.grid(row=8, column =1)
-e4.grid(row=9, column =1)
-e5.grid(row=10, column =1)
-e6.grid(row=11, column =1)
+e2.grid(row=11, column =1)
+e2.insert(0, "2017-01-01")
+e3.grid(row=12, column =1)
+e3.insert(0, "2017-12-31")
+
+# e4 replace to combobox
+Date_Format = {'20211231' : '%Y%m%d','2021-12-31': '%Y-%m-%d','2021/12/31': '%Y/%m/%d','12/31/2021':'%m/%d/%Y'}
+combobox13 = ttk.Combobox(win,height =5, state="readonly", values = list(Date_Format.keys()))
+combobox13.grid(row = 13, column = 1)
+combobox13.set("Select Date Format")
+
+# e5 replace to combobox
+NA_lines =  [str(i) for i in range(1,32)]
+combobox14 = ttk.Combobox(win,height =5, state="readonly", values = NA_lines)
+combobox14.grid(row = 14, column = 1)
+combobox14.set("Select column for NA")
+
+
+# e6 replace to combobox
+ACCT_B01 =  [str(i) for i in range(1,32)]
+combobox15 = ttk.Combobox(win,height =5, state="readonly", values = ACCT_B01)
+combobox15.grid(row = 15, column = 1)
+combobox15.set("Select column for B01")
 
 
 
-b1 = Button(win, text = "A01(1)",command=A1_1)
-b2 = Button(win, text = "A01(2)",command=A1_2)
-b3 = Button(win, text = "A02",command=A2)
-b4 = Button(win, text = "A03",command=A3)
-b5 = Button(win, text = "B01",command=B1)
+b22 = Button(win, text = "A01(1)", command=A1_1)
+b23 = Button(win, text = "A01(2)", command=A1_2)
+b24 = Button(win, text = "A02", command=A2)
+b25 = Button(win, text = "A03", command=A3)
+b26 = Button(win, text = "B01", command=B1)
+b50 = Button(win, text = "Save JE_Test File", command=B1)  # attach command
 
+b22.grid(row=22,column =0, sticky = N+E+W+S)
+b23.grid(row=23,column =0, sticky = N+E+W+S)
+b24.grid(row=24,column =0, sticky = N+E+W+S)
+b25.grid(row=25,column =0, sticky = N+E+W+S)
+b26.grid(row=26,column =0, sticky = N+E+W+S)
+b50.grid(row=50,column =0, sticky = N+E+W+S, columnspan =2)
 
-
-b1.grid(row=22,column =0)
-b2.grid(row=23,column =0)
-b3.grid(row=24,column =0)
-b4.grid(row=25,column =0)
-b5.grid(row=26,column =0)
 
 
 
@@ -192,7 +279,7 @@ lbl4.grid(row=24, column=1, sticky = W)
 lbl5 = Label(win, text ="Je TB Diff. EA : ? ")
 lbl5.grid(row=25, column=1, sticky = W)
 
-lbl6 = Label(win, text ="Corr EA : ? ")
+lbl6 = Label(win, text ="Corr. ACCT Code EA : ? ")
 lbl6.grid(row=26, column=1, sticky = W)
 
 
--- JE_Tester/test.py
+++ JE_Tester/test.py
@@ -1,2 +1,3 @@
-import calendar
-print(calendar.calendar(2020))
+Date_Format = {'20201231' : '%Y%m%d','2020-12-31': '%Y-%m-%d','2020/12/31': '%Y/%m/%d','12/31/2020':'%m/%d/%Y'}
+Date_Format.keys()
+Date_Format['20201231']
